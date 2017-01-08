5560ae1141f9fcdba77d7b561be844b64da8f244
Constantin <constantin.jean2111@gmail.com>
2017-01-09 00:30:47 +0100

    Template Matching works

--- Template_matching.py
+++ Template_matching.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys
 import os
 
+import contours as ctr
+
 base = cv2.imread('arial.png')
 charPath ="chars"
 
@@ -44,8 +46,8 @@ while (len(rectangles) > 0):
 			del rectangles[i]
 	results = results + [pom]
 print (results)
-cv2.imshow('base', base)
-cv2.waitKey(0)
+#cv2.imshow('base', base)
+#cv2.waitKey(0)
 
 folders = os.listdir(charPath)
 
@@ -71,7 +73,39 @@ for i in range(0, len(results)):
                     cv2.imshow('analysed',ROI)
                     cv2.waitKey(0)
 
+imBase = cv2.imread('BaseminFinal.png')
+imTest = cv2.imread('lorem.png')
 
+samples, responses, ls, sp, rectangles= ctr.CreateBase('BaseminFinal.png')
 
+# we use the same method for the other document we want to test.
+samplesTest, r, linesIndices, spaces, rectanglesTest = ctr.CreateBase('lorem.png')
+
+for rectT in rectanglesTest:
+    zone = imTest[rectT[1]: (rectT[1] + rectT[3]), rectT[0]: (rectT[0] + rectT[2])]
+    ROI = cv2.resize(zone, (21, 21), cv2.INTER_CUBIC)
+    #cv2.imshow('zone', ROI)
+    #cv2.waitKey(0)
+    first = True
+    tempMinVal = None
+    tmpTemplate = None
+    for rect in rectangles:
+        template = imBase[rect[1]: (rect[1] + rect[3]), rect[0]: (rect[0] + rect[2])]
+        templateResized = cv2.resize(template, (20, 20), cv2.INTER_CUBIC)
+        res = cv2.matchTemplate(ROI, templateResized, cv2.TM_SQDIFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        if first == True:
+            tempMinVal = min_val
+            tmpTemplate = template
+            first = False
+        elif min_val < tempMinVal:
+            tempMinVal = min_val
+            tmpTemplate = template
+ 
+    cv2.imshow('analysed',zone)
+    cv2.waitKey(0)
+    cv2.imshow('found',tmpTemplate)
+    cv2.waitKey(0)
+            
 
     
